03e1aa5df213e89afdf065437137bedfe7349bf8
xinhanlim <xhxhan00@gmail.com>
2025-06-20 06:47:11 +0000

    change header to arrows funcio with props

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
-export default function Header(props){
+const Header = (props) =>{
     return (
         <>
-          <header className="bg-primary text-white text-center py-5">
+         <header className="bg-primary text-white text-center py-5">
       <div className="container">
         <h1 className="display-4">Welcome to E-Shop</h1>
         <p className="lead">Discover amazing products at unbeatable prices!</p>
@@ -11,3 +11,19 @@ export default function Header(props){
         </>
     )
 }
+
+export default Header;
+
+// export default function Header(props){
+//     return (
+//         <>
+//           <header className="bg-primary text-white text-center py-5">
+//       <div className="container">
+//         <h1 className="display-4">Welcome to E-Shop</h1>
+//         <p className="lead">Discover amazing products at unbeatable prices!</p>
+//         <a href="#" className="btn btn-light btn-lg">Shop Now</a>
+//       </div>
+//     </header>
+//         </>
+//     )
+// }
