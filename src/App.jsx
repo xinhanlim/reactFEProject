9eb16592923567229eda19306af009a3cba2cc95
xinhanlim <xhxhan00@gmail.com>
2025-06-20 05:46:48 +0000

    complete className for whole landing page

--- src/App.jsx
+++ src/App.jsx
@@ -105,6 +105,12 @@ export default function App(){
         </div>
       </div>
     </main>
+
+    <footer className="bg-dark text-white text-center py-3">
+      <div className="container">
+        <p>&copy; 2023 E-Shop. All rights reserved.</p>
+      </div>
+    </footer>
     </>
   )
 }
