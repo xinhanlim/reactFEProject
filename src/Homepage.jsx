5c6712596779415b23b8cd8c49f90f00201abdf5
xinhanlim <xhxhan00@gmail.com>
2025-06-28 06:24:38 +0000

    compelted homepage section with .map

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,67 +4,58 @@ import ProductCard from './ProductCard'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-export default function Homepage(){
+export default function Homepage() {
 
   const { featuredProducts, setFeaturedProducts } = useState([]);
 
-  useEffect(()=> {
-    try{
+  useEffect(() => {
+    try {
       const getFeaturedProducts = async () => {
-      const response = await axios.get('./featured.json');
-      setFeaturedProducts(response.data);
+        const response = await axios.get('./featured.json');
+        setFeaturedProducts(response.data);
       }
-
       getFeaturedProducts();
 
-    }catch(e){
+    } catch (e) {
       console.log("error");
     }
   }, [])
-
-    const renderFeaturedProduct = () => {
-      const featuredProduct = [];
-      for (product of featuredProduct) {
-        featuredProduct.push()
-      }
-    }
-
-    return (
-    <>
-    <Header/>
-    <main className="container my-5">
-      <h2 className="text-center mb-4">Featured Products</h2>
-      <div className="row">
-        <div className="col-md-3 mb-4">
-          <ProductCard
-          imageUrl = "https://picsum.photos/id/20/300/200"
-          productName = "Product 1"
-          price = {19.99}
-          />
-        </div>
-        <div className="col-md-3 mb-4">
-          <ProductCard
-          imageUrl = "https://picsum.photos/id/1/300/200"
-          productName = "Product 2"
-          price = {29.99}
-          />
-        </div>
-        <div className="col-md-3 mb-4">
-          <ProductCard
-          imageUrl = "https://picsum.photos/id/26/300/200"
-          productName = "Product 3"
-          price = {39.99}
-          />
-        </div>
-        <div className="col-md-3 mb-4">
-          <ProductCard
-          imageUrl = "https://picsum.photos/id/33/300/200"
-          productName = "Product 1"
-          price = {49.99}
-          />
-        </div>
+return(
+<>
+  <Header />
+  <main className="container my-5">
+    <h2 className="text-center mb-4">Featured Products</h2>
+    <div className="row">
+      <div className="col-md-3 mb-4">
+        <ProductCard
+          imageUrl="https://picsum.photos/id/20/300/200"
+          productName="Product 1"
+          price={19.99}
+        />
       </div>
-    </main>
-    </>
-    )
+      <div className="col-md-3 mb-4">
+        <ProductCard
+          imageUrl="https://picsum.photos/id/1/300/200"
+          productName="Product 2"
+          price={29.99}
+        />
+      </div>
+      <div className="col-md-3 mb-4">
+        <ProductCard
+          imageUrl="https://picsum.photos/id/26/300/200"
+          productName="Product 3"
+          price={39.99}
+        />
+      </div>
+      <div className="col-md-3 mb-4">
+        <ProductCard
+          imageUrl="https://picsum.photos/id/33/300/200"
+          productName="Product 1"
+          price={49.99}
+        />
+      </div>
+    </div>
+  </main>
+</>
+)
 }
--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-
-
+import React, { useEffect } from 'react'
+import {useState, useeffect} from 'react'
+import axios from 'axios'
+import ProductCard from './ProductCard'
 
 
 export default function ProductPage() {
+
+    const {products, setProducts} = useState([]);
+
+    useEffect(()=> {
+      const fetchProducts = async ()=> {
+        try {
+          const response = await axios.get('./products.json');
+          setProducts(response.data);
+        } catch (e){
+          console.log("Error")
+        }
+      }
+      fetchProducts();
+    }, [])
+      console.log(setProducts);
     return (
     <>
     <div className="container mt-5">
       <h1>Our Products</h1>
       <p>This is where we'll display our product catalog.</p>
+
+    {/* {
+      products.map(p => (
+       <ProductCard 
+        image = {p.image}
+        name = {p.name}
+        price = {p.price.toFixed(2)}
+       />
+      )
+    )
+    } */}
+
     </div>
+
     </>
     )
 }
